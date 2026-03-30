ba96b7750fe33129810ab2bd0768f6da2a4236f6
fanfan <3243635662@qq.com>
2026-03-30 18:33:00 +0800

    feat: initialize new Nuxt project with base configuration and VS Code settings

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  devServer: {
+    port: 5173, // 将端口改为你想要的，例如 5173
+  },
+});
